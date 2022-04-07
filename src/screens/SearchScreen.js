aab0e6c5068aafa20f44edd78be2dab00dffbc80
BeanBeam <s110819015@stu.ntue.edu.tw>
2022-04-07 14:47:03 +0800

    import map

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,15 +1,80 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Dimensions, Platform } from 'react-native';
 import { useColorMode, Box, Text} from 'native-base';
+import MapView, { Marker } from 'react-native-maps';
+import Constants from "expo-constants";
+import * as Location from "expo-location";
 
 const SearchScreen = () => {
+    const [ location, setLocation ] = useState(null);
+    const [ errorMsg, setErrorMsg ] = useState(null);
+    const [ region, setRegion ] = useState({
+        longitude: 121.544637,
+        latitude: 25.024624,
+        longitudeDelta: 0.01,
+        latitudeDelta: 0.02,
+    });
+    const [ marker, setMarker ] = useState({
+        coord: {
+          longitude: 121.544637,
+          latitude: 25.024624,
+        },
+        name: "國立臺北教育大學",
+        address: "台北市和平東路二段134號",
+    });
+
+    useEffect(() => {
+        getLocation();
+    });
+
+    const getLocation = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+            return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+    };
+
+    const onRegionChangeComplete = (rgn) => {
+        if(
+            Math.abs(rgn.latitude - region.latitude) > 0.0002 ||
+            Math.abs(rgn.longitude - region.longitude) > 0.0002
+        ){
+            setRegion(rgn);
+            setMarker({
+                ...marker,
+                coord: {
+                    longitude: rgn.longitude,
+                    latitude: rgn.latitude,
+                },
+            });
+        }
+    };
+
     return(
         <Box
             style={styles.container}
             _dark={{ bg: "#484848"}}
             _light={{ bg: "#fff"}}
         >
-            <Text>Search</Text>
+            <MapView
+                style={styles.map}
+                region={region}
+                onRegionChangeComplete={onRegionChangeComplete}
+                showsTraffic
+                provider='google'
+            >
+                <Marker
+                    coordinate={marker.coord}
+                    title={marker.name}
+                    description={marker.address}
+                >
+                    {/* <View></View> */}
+                </Marker>
+            </MapView>
         </Box>
     );
 }
@@ -21,5 +86,10 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    map: {
+        flex: 1,
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
     },
 });
