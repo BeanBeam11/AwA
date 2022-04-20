e67d4d8fe195395a34a613aaf2124dbc8d31565d
BeanBeam <s110819015@stu.ntue.edu.tw>
2022-04-20 21:55:11 +0800

    refactor navigation

---
+++ src/navigation/MyStacks.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from '../screens/HomeScreen';
+import SearchScreen from '../screens/SearchScreen';
+import PlannerScreen from '../screens/PlannerScreen';
+import PlanDetailScreen from '../screens/PlanDetailScreen';
+import ShareScreen from '../screens/ShareScreen';
+import AccountScreen from '../screens/AccountScreen';
+import SettingScreen from '../screens/SettingScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import ProfileEditScreen from '../screens/ProfileEditScreen';
+
+const Stack = createNativeStackNavigator();
+
+export const HomeStack = () => {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        </Stack.Navigator>
+    );
+};
+
+export const SearchStack = () => {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen name="SearchScreen" component={SearchScreen} />
+        </Stack.Navigator>
+    );
+};
+
+export const PlannerStack = () => {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen name="PlannerScreen" component={PlannerScreen} />
+            <Stack.Screen name="PlanDetailScreen" component={PlanDetailScreen} />
+        </Stack.Navigator>
+    );
+};
+
+export const ShareStack = () => {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen name="ShareScreen" component={ShareScreen} />
+        </Stack.Navigator>
+    );
+};
+
+export const AccountStack = () => {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen name="AccountScreen" component={AccountScreen} />
+            <Stack.Screen name="SettingScreen" component={SettingScreen} />
+            <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+            <Stack.Screen
+                name="ProfileEditScreen"
+                component={ProfileEditScreen}
+                options={({ navigation }) => ({
+                    presentation: 'fullScreenModal',
+                })}
+            />
+        </Stack.Navigator>
+    );
+};
---
+++ src/navigation/MyTabs.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import { StyleSheet, Image } from 'react-native';
+import { useTheme } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useColorMode } from 'native-base';
+
+import { HomeStack, SearchStack, PlannerStack, ShareStack, AccountStack } from './MyStacks';
+
+const Tab = createBottomTabNavigator();
+
+export const MyTabs = () => {
+    const { colorMode } = useColorMode();
+    const { colors } = useTheme();
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+
+                    if (route.name === 'Home') {
+                        if ((iconName = focused)) {
+                            return (
+                                <Image
+                                    source={require('../../assets/icons/ic_home_actived.png')}
+                                    style={styles.navIcon}
+                                />
+                            );
+                        } else {
+                            return colorMode === 'dark' ? (
+                                <Image source={require('../../assets/icons/ic_home_dark.png')} style={styles.navIcon} />
+                            ) : (
+                                <Image source={require('../../assets/icons/ic_home.png')} style={styles.navIcon} />
+                            );
+                        }
+                    } else if (route.name === 'Search') {
+                        if ((iconName = focused)) {
+                            return (
+                                <Image
+                                    source={require('../../assets/icons/ic_map_actived.png')}
+                                    style={styles.navIcon}
+                                />
+                            );
+                        } else {
+                            return colorMode === 'dark' ? (
+                                <Image source={require('../../assets/icons/ic_map_dark.png')} style={styles.navIcon} />
+                            ) : (
+                                <Image source={require('../../assets/icons/ic_map.png')} style={styles.navIcon} />
+                            );
+                        }
+                    } else if (route.name === 'Planner') {
+                        if ((iconName = focused)) {
+                            return (
+                                <Image
+                                    source={require('../../assets/icons/ic_planner_actived.png')}
+                                    style={styles.navIcon}
+                                />
+                            );
+                        } else {
+                            return colorMode === 'dark' ? (
+                                <Image
+                                    source={require('../../assets/icons/ic_planner_dark.png')}
+                                    style={styles.navIcon}
+                                />
+                            ) : (
+                                <Image source={require('../../assets/icons/ic_planner.png')} style={styles.navIcon} />
+                            );
+                        }
+                    } else if (route.name === 'Share') {
+                        if ((iconName = focused)) {
+                            return (
+                                <Image
+                                    source={require('../../assets/icons/ic_share_actived.png')}
+                                    style={styles.navIcon}
+                                />
+                            );
+                        } else {
+                            return colorMode === 'dark' ? (
+                                <Image
+                                    source={require('../../assets/icons/ic_share_dark.png')}
+                                    style={styles.navIcon}
+                                />
+                            ) : (
+                                <Image source={require('../../assets/icons/ic_share.png')} style={styles.navIcon} />
+                            );
+                        }
+                    } else if (route.name === 'Account') {
+                        if ((iconName = focused)) {
+                            return (
+                                <Image
+                                    source={require('../../assets/icons/ic_account_actived.png')}
+                                    style={styles.navIcon}
+                                />
+                            );
+                        } else {
+                            return colorMode === 'dark' ? (
+                                <Image
+                                    source={require('../../assets/icons/ic_account_dark.png')}
+                                    style={styles.navIcon}
+                                />
+                            ) : (
+                                <Image source={require('../../assets/icons/ic_account.png')} style={styles.navIcon} />
+                            );
+                        }
+                    }
+
+                    return iconName;
+                },
+                headerShown: false,
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    backgroundColor: colorMode == 'dark' ? colors.dark100 : '#fff',
+                    borderTopWidth: 0,
+                    borderTopRightRadius: 20,
+                    borderTopLeftRadius: 20,
+                    position: 'absolute',
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                },
+            })}
+        >
+            <Tab.Screen name="Home" component={HomeStack} />
+            <Tab.Screen name="Search" component={SearchStack} />
+            <Tab.Screen name="Planner" component={PlannerStack} />
+            <Tab.Screen name="Share" component={ShareStack} />
+            <Tab.Screen
+                name="Account"
+                component={AccountStack}
+                options={({ navigation }) => ({
+                    tabBarStyle: { display: 'none' },
+                })}
+            />
+        </Tab.Navigator>
+    );
+};
+
+const styles = StyleSheet.create({
+    navIcon: {
+        width: 24,
+        height: 24,
+    },
+});
